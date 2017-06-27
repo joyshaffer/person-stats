f6b8eec0bea43ec3555c7416ab16d31df03a858b
Joy Shaffer <jeshaffer@anderson.edu>
2017-06-27 19:57:27 -0400

    Fixed color

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-function changeHeading(ev) {
-    ev.preventDefault()
-    const f = event.target
-    const name = f.personName.value 
-    const age = f.personAge.value
-    const color = f.textColor.value
-    const div = document.querySelector('#stats')
+function handleSubmit(ev) {
+  ev.preventDefault()
+  const f = ev.target
+  const name = f.personName.value
+  const age = f.personAge.value
+  const color = f.favoriteColor.value
 
-    const p = document.createElement('p')
-    p.textContent = `<p>${name}, age ${age}</p>`
-    p.style.color = color
-    div.appendChild(p)
+  const div = document.querySelector('#stats')
+  
+  const list = document.createElement('ul')
 
-    //div.innerHTML = name
-    //***div.innerHTML = '<p>' + name + ', age ' + age + '</p>'    
-    //div.innerHTML = `<p style="color: ${color}">${name}, age ${age}</p>`
-    //gets own paragraph
+  const nameItem = document.createElement('li')
+  nameItem.textContent = `Name: ${name}`
+  list.appendChild(nameItem)
+
+  const ageItem = document.createElement('li')
+  ageItem.textContent = `Age: ${age}`
+  list.appendChild(ageItem)
+
+  const colorItem = document.createElement('li')
+  colorItem.textContent = 'Favorite Color: '
+
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = color
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+  colorItem.appendChild(colorDiv)
+
+  list.appendChild(colorItem)
+
+  div.appendChild(list)
+
+  // div.innerHTML = '<p>' + name + ', age ' + age + '</p>'
+  // div.innerHTML = `<p style="background-color: ${color}">${name}, age ${age}</p>`
 }
 
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', changeHeading)
-
-
+personForm.addEventListener('submit', handleSubmit)
