2b566c3604d02a472b5ac2394eacee792c6e5032
Joy Shaffer <jeshaffer@anderson.edu>
2017-06-26 21:03:02 -0400

    Update stats div

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ function changeHeading(ev) {
     const f = ev.target
     const name = f.personName.value
     const heading = document.querySelector('h1')
-    heading.textContent = name
+    heading.innerHTML = name
 }
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
+
+function updateDiv(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const name = f.personName.value
+    const heading = document.querySelector('#stats')
+    heading.innerHTML = name
+}
+
+const div = document.querySelector('#stats')
+div.addEventListener('submit', updateDiv)
